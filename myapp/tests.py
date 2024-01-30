aa1f4cbf30510da4e57cc9d31986b0d6eabe5fba
MohammadSatel <hamuds95@gmail.com>
2024-01-30 10:28:09 +0200

    debug

--- myapp/tests.py
+++ myapp/tests.py
@@ -20,27 +20,3 @@ class TestProductsView(TestCase):
         request = self.factory.get('/products/4/')  # Change the URL to match your routing
         response = view(request, catID=4)  # Change the catID to a valid category ID
         self.assertEqual(response.status_code, 200)  # Assuming 200 is the expected status code for success
-
-    # def test_post_product(self):
-        # view = ProductsView.as_view()
-        # data = {"desc": "Test Product", "price": 10.99, "ctg": "4"}
-        # request = self.factory.post('/products/',  _RequestData = json.dumps(data),str = 'application/json')
-        # response = view(request)
-        # self.assertEqual(response.status_code, 201)  # Assuming 201 is the expected status code for successful creation
-
-    # def test_put_product(self):
-    #     # Assuming an existing product ID for update
-    #     product_id = 1  # Change to a valid product ID
-    #     view = ProductsView.as_view()
-    #     data = {'name': 'Updated Product', 'price': 15.99}  # Change as needed
-    #     request = self.factory.put(f'/products/{product_id}/', data=json.dumps(data), content_type='application/json')
-    #     response = view(request)
-    #     self.assertEqual(response.status_code, 200)  # Assuming 200 is the expected status code for success
-
-    # def test_delete_product(self):
-    #     # Assuming an existing product ID for deletion
-    #     product_id = 1  # Change to a valid product ID
-    #     view = ProductsView.as_view()
-    #     request = self.factory.delete(f'/products/{product_id}/')
-    #     response = view(request)
-    #     self.assertEqual(response.status_code, 204)  
--- myapp/urls.py
+++ myapp/urls.py
@@ -7,16 +7,16 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('register', views.register_or_update_user),
+    path('register/', views.register_or_update_user),
     path('login/', views.MyTokenObtainPairView.as_view()),
     path('logout/', auth_views.LogoutView.as_view()),
-    path('forgotpass', views.forgot_password, name='forgot_password'),
+    path('forgotpass/', views.forgot_password, name='forgot_password'),
     path('products/', views.ProductsView.as_view()),
-    path('categories', views.CategoriesView.as_view()),
-    path('checkout', views.checkOut),
+    path('categories/', views.CategoriesView.as_view()),
+    path('checkout/', views.checkOut),
     path('products/<int:catID>', views.ProductsView.as_view()),
-    path('history', views.get_orders),
-    path('upduser', views.update_user_details),
+    path('history/', views.get_orders),
+    path('upduser/', views.update_user_details),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
